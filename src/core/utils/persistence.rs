5d63b1a2f8909f25e1cd502f943cc17f6a5b4c0b
rudr4x5h <pathak.adarsh@gmail.com>
2025-02-06 17:43:14 +0530

    feat: implementing db connectors; working on #8.

--- src/core/utils/persistence.rs
+++ src/core/utils/persistence.rs
@@ -1,1 +1,23 @@
+use std::sync::LazyLock;
+use surrealdb::{
+    engine::remote::ws::{Client, Ws},
+    opt::auth::Root,
+    Surreal,
+};
 
+use super::error::AppError;
+
+pub static DB: LazyLock<Surreal<Client>> = LazyLock::new(Surreal::init);
+
+pub async fn init_db_connection() -> Result<(), AppError> {
+    DB.connect::<Ws>("localhost:8000").await?;
+    DB.signin(Root {
+        username: "root",
+        password: "root",
+    })
+    .await?;
+
+    DB.use_ns("testing").use_db("db_test").await?;
+    println!("DB connection established.");
+    Ok(())
+}
