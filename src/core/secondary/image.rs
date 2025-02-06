1ae0e28a12f31f68e8cc211b648709e5946b05f3
rudr4x5h <pathak.adarsh@gmail.com>
2025-02-06 18:05:26 +0530

    chore: adding debug for triaging purpose.

--- src/core/secondary/image.rs
+++ src/core/secondary/image.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use ulid::Ulid;
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Image {
     id: Ulid,
     uri: String,
