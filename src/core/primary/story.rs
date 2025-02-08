5dc529ea2e6f5d1b18dc723832af4689aa923f71
rudr4x5h <pathak.adarsh@gmail.com>
2025-02-08 13:05:34 +0530

    feat: implementing basic search.

--- src/core/primary/story.rs
+++ src/core/primary/story.rs
@@ -30,6 +30,19 @@ impl StoryWithId {
     pub fn get_body(&self) -> &Body {
         &self.body
     }
+
+    pub fn get_story_instance(&self) -> Story {
+        Story {
+            created_at: self.created_at,
+            updated_at: self.updated_at,
+            author: self.author.clone(),
+            domain: self.domain.clone(),
+            body: self.body.clone(),
+            headline: self.headline.clone(),
+            synopsis: self.synopsis.clone(),
+            headshot: self.headshot.clone(),
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
